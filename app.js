017cae33bce8d622d85b208419dc89eb9e04595e
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-05-27 01:16:02 +0530

    Header with css

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
-const Title=()=>{
-   return( <h1>Title</h1>)
-}
-const Header=()=>{
-    return(
-      <div>
-        <Title/>
-        <div className="head">
-          <ul>
-            <li>home</li>
-            <li>about</li>
-            <li>contact</li>
-          </ul>
-        </div>
-  
+import ReactDOM from "react-dom/client";
+const Title = () => {
+  return (
+   <a href="/"> 
+   <img
+    src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-healthy-food-logo-fork-with-green-leaves-decoration-vector-png-image_3773921.jpg" alt="just j1"
+    id="imglogo"
+    ></img>
+    </a>
+  );
+};
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <Title />
+        <ul>
+          
+          <li>about</li>
+          <li>contact</li>
+          <li>cart</li>
+        </ul>
       </div>
-    );y
-  }
-  const Body= ()=>{
-   
-  }
-  const Footer= ()=>{
-   
-  }
+    </div>
+  );
   
-  const AppLayout =()=>{
-    return(<React.Fragment>
-      <Header/>
-      <Body/>
-      <Footer/>
-      
-    </React.Fragment>)
-  }
-  
+};
+const Body = () => {
+ return (<>
+    <h1>Body</h1>
+  </>)
+};
+const Footer = () => {
+ return( <>
+    <h1>Footer</h1>
+  </>)
+};
 
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<AppLayout />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
