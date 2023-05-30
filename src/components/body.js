808f89056496a5054186e1c2cb830193c2a68220
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-05-31 02:14:56 +0530

    search filter update

--- src/components/body.js
+++ src/components/body.js
@@ -7,23 +7,39 @@ const link =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 const Card = (props) => {
-  const { cloudinaryImageId, name, cuisines, avgRating } = props;
+  if (props.message) {
+    return (
+      <div className="resCard">
+        <img src=""></img>
+        <h2>{props.message}</h2>
+        <h3></h3>
+        <h4></h4>
+      </div>
+    );
+  } else {
+    const { cloudinaryImageId, name, cuisines, avgRating } = props;
 
-  return (
-    <div className="resCard">
-      <img src={link + cloudinaryImageId}></img>
-      <h2>{name}</h2>
-      <h3>{cuisines.join(", ")}</h3>
-      <h4>rating : {avgRating}</h4>
-    </div>
-  );
+    return (
+      <div className="resCard">
+        <img src={link + cloudinaryImageId}></img>
+        <h2>{name}</h2>
+        <h3>{cuisines.join(", ")}</h3>
+        <h4>rating : {avgRating}</h4>
+      </div>
+    );
+  }
 };
 
 function filterData(serchText, restaurant) {
-  let data = restaurant.filter((res) =>
-    res?.data?.data?.name.toLowerCase().includes(serchText.toLowerCase())
-  );
-
+  let data = restaurant.filter((res) => {
+    if (!res?.data?.data?.message) {
+      //console.log(res?.data?.data?.name)
+      return res?.data?.data?.name
+        .toLowerCase()
+        .includes(serchText.toLowerCase());
+    }
+  });
+  console.log(data);
   return data;
 }
 
@@ -33,15 +49,24 @@ const Body = () => {
   let [serchText, setSearchInput] = useState("");
 
   useEffect(() => {
+    console.log("api call");
     getSwiggyData();
   }, []);
+
+  useEffect(() => {
+    console.log("searching");
+    const data = filterData(serchText, restaurants);
+
+    setFilteredRestaurants(data);
+  }, [serchText]);
 
   async function getSwiggyData() {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.66682693229555&lng=74.25015181303024&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
     );
     const jsonData = await data.json();
-    // console.log(jsonData.data.cards)
+    //console.log(jsonData.data.cards)
+    console.log(jsonData.data.cards[7].data.data);
     setRestaurant(jsonData?.data?.cards);
     setFilteredRestaurants(jsonData?.data?.cards);
   }
@@ -74,7 +99,10 @@ const Body = () => {
       <div className="resList">
         {filteredRestaurants.map((restorunt) => {
           return (
-            <Card {...restorunt?.data?.data} key={restorunt?.data?.data?.id} />
+            <Card
+              {...restorunt?.data?.data}
+              key={restorunt?.data?.data?.id || Math.random()}
+            />
           );
         })}
       </div>
